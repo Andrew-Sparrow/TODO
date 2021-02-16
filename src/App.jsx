91ec20d5a62bfad9a47174cae1d1c5778f8f29b0
Andrew <8911102@gmail.com>
2021-02-16 11:28:30 +0500

    axios was added for toggleCompleted task

--- src/App.jsx
+++ src/App.jsx
@@ -73,19 +73,11 @@ const App = () => {
     const taskToToggle = await fetchTask(id);
     const updatedTask = { ...taskToToggle, isCompleted: isChecked };
 
-    const response = await fetch(`${process.env.REACT_APP_API_PATH}:${process.env.REACT_APP_API_PORT}/tasks/${id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify(updatedTask),
-    });
-
-    const taskFromServer = await response.json();
+    const response = await axios.put(`/tasks/${id}`, JSON.stringify(updatedTask));
 
     setTasks(tasks.map(
       (task) => (task.id === id
-        ? { ...task, isCompleted: taskFromServer.isCompleted }
+        ? { ...task, isCompleted: response.data.isCompleted }
         : task),
     ));
   };
